c61b35bde134b2f71a2bb59446e8f0f5cbb904b6
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2022-04-01 01:50:29 +0530

    chore: minor housekeeping ✅

--- ship.config.cjs
+++ ship.config.cjs
@@ -2,5 +2,5 @@ module.exports = {
   installCommand: () => 'npm i --prefer-offline --no-audit --no-optional',
   buildCommand: () => null,
   publishCommand: ({ tag }) =>
-    `echo "Releasing ${tag} version of Expenses frontend webapp"`,
+    `echo "Releasing ${tag} version of ffmpegwasm :: Nuxt App"`,
 };
